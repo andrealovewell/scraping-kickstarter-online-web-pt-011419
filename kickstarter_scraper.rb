7369d10b2480ca29a0f92e43ba0f931d27d5897e
Andrea Lovewell <andy.lovewell@gmail.com>
2019-03-05 04:31:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,19 @@
 # require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+end
+
+projects = {}
+
+kickstarter.css("li.project.grid_4").each do |project|
+  projects[project] ={}
+end
+
+  projects
+  end
+end
+
 end
